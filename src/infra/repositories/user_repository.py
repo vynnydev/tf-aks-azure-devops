7925e79fa558ce0489786a496f12cfac906dff09
Vinicius Diniz Carlos Prudencio <vynnyprudencio@gmail.com>
2022-10-06 16:45:15 -0300

    feat: add select user implementation and test method in user

--- src/infra/repositories/user_repository.py
+++ src/infra/repositories/user_repository.py
@@ -1,14 +1,16 @@
 # pylint: disable=E1101
-from src.domain.models import Users as UserModel
+from typing import List
+from sqlalchemy.orm.exc import NoResultFound
+from src.domain.models import Users as UsersModel
 from src.infra.config import DBConnectionHandler
-from src.infra.entities import Users as UserEntity
+from src.infra.entities import Users as UsersEntity
 
 
 class UserRepository:
     """Class to manage User Repository"""
 
     @classmethod  # especificando como método de classe
-    def insert_user(cls, name: str, password: str) -> UserModel:
+    def insert_user(cls, name: str, password: str) -> UsersModel:
         """insert data in user entity
         :param - name: person name
                - password: user pasword
@@ -17,16 +19,70 @@ class UserRepository:
 
         with DBConnectionHandler() as db_connection:
             try:
-                new_user = UserEntity(name=name, password=password)
+                new_user = UsersEntity(name=name, password=password)
                 db_connection.session.add(new_user)
                 db_connection.session.commit()
 
-                return UserModel(
+                return UsersModel(
                     id=new_user.id, name=new_user.name, password=new_user.password
                 )
             except:
                 db_connection.session.rollback()
+                raise
             finally:
                 db_connection.session.close()
+
+        return None
+
+    @classmethod
+    def select_user(cls, user_id: int = None, name: str = None) -> List[UsersModel]:
+        """
+        Select data in user entity by id and/or name
+        :param - user_id: Id of the registry
+               - name: User name
+        :return - List with Users selected
+        """
+
+        try:
+            query_data = None
+
+            if user_id and not name:
+
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(UsersEntity)
+                        .filter_by(id=user_id)
+                        .one()
+                    )
+                    query_data = [data]
+
+            elif not user_id and name:
+
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(UsersEntity)
+                        .filter_by(name=name)
+                        .one()
+                    )
+                    query_data = [data]
+
+            elif user_id and name:
+
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(UsersEntity)
+                        .filter_by(id=user_id, name=name)
+                        .one()
+                    )
+                    query_data = [data]
+
+            return query_data
+
+        except NoResultFound:
+            return []
+        except:
+            db_connection.session.rollback()
+        finally:
+            db_connection.session.close()
 
         return None
