c465e5427bfabf5e2423ee687de4d078d5fcf692
Vinicius Diniz Carlos Prudencio <vynnyprudencio@gmail.com>
2022-10-06 16:05:01 -0300

    feat: add user model in domain layer

--- src/infra/repositories/user_repository.py
+++ src/infra/repositories/user_repository.py
@@ -1,29 +1,27 @@
 # pylint: disable=E1101
-from collections import namedtuple
+from src.domain.models import Users as UserModel
 from src.infra.config import DBConnectionHandler
-from src.infra.entities import Users
+from src.infra.entities import Users as UserEntity
 
 
 class UserRepository:
     """Class to manage User Repository"""
 
     @classmethod  # especificando como método de classe
-    def insert_user(cls, name: str, password: str) -> Users:
+    def insert_user(cls, name: str, password: str) -> UserModel:
         """insert data in user entity
         :param - name: person name
                - password: user pasword
         :return - tuple with new user inserted
         """
 
-        insertData = namedtuple("Users", "id, name, password")
-
         with DBConnectionHandler() as db_connection:
             try:
-                new_user = Users(name=name, password=password)
+                new_user = UserEntity(name=name, password=password)
                 db_connection.session.add(new_user)
                 db_connection.session.commit()
 
-                return insertData(
+                return UserModel(
                     id=new_user.id, name=new_user.name, password=new_user.password
                 )
             except:
